9383c8dd0b41ddca3a69320577cdedae9c5d3d98
WIse <145248257+Sayrrexe@users.noreply.github.com>
2024-09-30 19:00:15 +0500

    A

--- Food-Portal/recepts/views.py
+++ Food-Portal/recepts/views.py
@@ -5,7 +5,8 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from .models import Recept, Author
+from .models import Recept, Author, Comment
+from .forms import CommentForm
 
 
 class ReceptsList(ListView):
@@ -22,8 +23,10 @@ class ReceptsList(ListView):
         return context
     
 class RecipeDetail(DetailView):
+    form_class = CommentForm    
     model = Recept
     template_name = 'recipe.html'
+    
     context_object_name = 'recipe'
     
 @method_decorator(login_required, name='dispatch')
@@ -37,4 +40,23 @@ class RecipeCreate(CreateView):
         author, created = Author.objects.get_or_create(author_user=self.request.user)  # 
         form.instance.author = author
         return super().form_valid(form)
+    
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = CommentForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = CommentForm()
+
+    return 
     
